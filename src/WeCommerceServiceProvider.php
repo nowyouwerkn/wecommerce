f236f3175a6e493be8f928cb2b44d386367aab93
jorgeepena <jorgepena@aragorn-elessar.local>
2021-07-10 13:56:32 -0500

    Cambios en Service Provider

--- src/WeCommerceServiceProvider.php
+++ src/WeCommerceServiceProvider.php
@@ -13,8 +13,39 @@ class WeCommerceServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\AuthController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\BannerController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\CartController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\CategoryController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\CityController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\ClientController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\CountryController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\CouponController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\CurrencyController');
         $this->app->make('Nowyouwerkn\WeCommerce\Controllers\DashboardController');
-        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\FrontController');        
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\ExampleController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\FrontController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\InstallController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\LegalTextController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\LogController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\MailController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\NotificationController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\OrderController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\OrderNoteController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\PaymentMethodController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\ProductController');       
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\ProductVariantController'); 
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\ReviewController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\SearchController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\SEOController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\ShipmentMethodController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\StateController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\StockController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\StoreConfigController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\StoreTaxController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\UserController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\VariantController');
+        $this->app->make('Nowyouwerkn\WeCommerce\Controllers\WishlistController');
     }
 
     /**
@@ -24,10 +55,17 @@ class WeCommerceServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->publishResources();
+        }
+
         $this->loadRoutesFrom(__DIR__.'/routes.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/views', 'wecommerce');
+    }
 
+    protected function publishResources()
+    {
         // Primera ruta es de donde viene el recurso a publicar y la segunda ruta en que parte se instalará.
         $this->publishes([
             __DIR__.'/views/front' => resource_path('views/front/theme/'),
