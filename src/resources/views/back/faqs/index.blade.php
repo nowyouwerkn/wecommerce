ab055f2bb75b9ed9969860ee2db1a9ea427d2762
noehassiel <hassiel-monterrosas@hotmail.com>
2022-05-30 17:27:45 -0500

    correcion en FAQS

--- src/resources/views/back/faqs/index.blade.php
+++ src/resources/views/back/faqs/index.blade.php
@@ -51,7 +51,6 @@
         <div class="card card-body text-center" style="padding:50px 0px 50px 0px;">
             <img src="{{ asset('assets/img/group.svg') }}" class="wd-20p ml-auto mr-auto mb-5">
             <h4>¡No hay textos preguntas frecuentes en la base de datos!</h4>
-            <a href="" class="btn btn-sm btn-primary btn-uppercase wd-200 ml-auto mr-auto mt-4">Agregar pregunta frecuente</a>
         </div>
         @else
 
