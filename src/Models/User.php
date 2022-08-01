b5f35347a87a377993fa262693d2578eb0a0d735
jorgeepena <jorge.pena@werkn.mx>
2022-08-01 18:24:57 -0500

    Los pedidos en el resumen de pedidos del usuario se orden por fecha de creación

--- src/Models/User.php
+++ src/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
 
     public function orders()
     {
-        return $this->hasMany(Order::class);
+        return $this->hasMany(Order::class)->orderBy('created_at', 'desc');
     }
 
     public function invoices()
