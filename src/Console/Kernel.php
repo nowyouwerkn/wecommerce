4625b5191a5b37b3e8fa3dc10db47b9866344c3c
noehassiel <hassiel-monterrosas@hotmail.com>
2022-10-04 17:43:29 -0500

    kernel update

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\DebugTest::class,
         Commands\GiveAccountPoints::class,
     ];
 
