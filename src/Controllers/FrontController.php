6400cba349f0a437c6e3b152c0d71d22d9cad35a
jorgeepena <eggotron@gmail.com>
2024-06-14 17:56:01 -0600

    Solución cambio decimal de Conekta

--- src/Controllers/FrontController.php
+++ src/Controllers/FrontController.php
@@ -1397,13 +1397,13 @@ class FrontController extends Controller
 
         $products[0] = array(
             'name' => 'Orden en tu Tienda en Línea',
-            'unit_price' => ($request->final_total . '.00'),
+            'unit_price' => ($request->final_total . '00'),
             'quantity' => '1'
         );
 
         $products[1] = array(
             'name' => 'Tarifa de envío',
-            'unit_price' => ($request->shipping_rate . '.00') ?? '0.00',
+            'unit_price' => ($request->shipping_rate . '00') ?? '0',
             'quantity' => '1'
         );
 
